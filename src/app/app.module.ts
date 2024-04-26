3c8e296fc48e4836a77812e3f25e615a8a03d1c2
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2024-04-26 06:50:57 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/presentation/login/login.component';
+import { RegisterComponent } from './auth/presentation/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
