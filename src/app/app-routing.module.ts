bc98c66f7388b382e3ce7a01f40cdf58ea0d26b0
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2024-04-29 10:18:25 -0500

    Update app-routing.module.ts
    Profile was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/presentation/login/login.component';
 import { RegisterComponent } from './auth/presentation/register/register.component';
+import { ProfileComponent } from './profile/presentation/profile/profile.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
   }
 ];
 
