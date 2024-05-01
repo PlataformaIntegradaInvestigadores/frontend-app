29f1c6b8d3855c5ae122c8ae68d9580a2554ac09
Danny Cabrera <78981957+DonOtame@users.noreply.github.com>
2024-05-01 16:00:03 -0500

    (add) nav data and data-nav component

--- src/app/profile/presentation/profile-page/profile-page.module.ts
+++ src/app/profile/presentation/profile-page/profile-page.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ProfilePageRoutingModule } from './profile-page-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileDataComponent } from './components/profile-data/profile-data.component';
+import { DataNavComponent } from './components/data-nav/data-nav.component';
 
 
 @NgModule({
   imports: [ProfilePageRoutingModule],
   exports: [],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    HeaderComponent,
+    ProfileDataComponent,
+    DataNavComponent
   ],
   providers: [],
 })
