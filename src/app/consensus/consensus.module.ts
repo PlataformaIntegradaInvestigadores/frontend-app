80bdc103ffe8f562ff2756ad612f93f2ac2fdab0
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2024-05-08 14:58:28 -0500

    feat(consensus): implement some components to consensus page phase 1

--- src/app/consensus/consensus.module.ts
+++ src/app/consensus/consensus.module.ts
@@ -3,14 +3,16 @@ import { RecommendTopicsPageComponent } from './presentation/pages/recommend-top
 import { ValuationPageComponent } from './presentation/pages/valuation-page/valuation-page.component';
 import { DecisionPageComponent } from './presentation/pages/decision-page/decision-page.component';
 import { ConsensusRoutingModule } from "./consensus-routing.module";
+import { ProfilePageModule } from "../profile/presentation/profile-page/profile-page.module";
 
 @NgModule({
     declarations: [
     RecommendTopicsPageComponent,
     ValuationPageComponent,
-    DecisionPageComponent
+    DecisionPageComponent,
+    
   ],
-    imports: [ConsensusRoutingModule],
+    imports: [ConsensusRoutingModule, ProfilePageModule],
     exports: []
 })
 export class ConsensusModule {
