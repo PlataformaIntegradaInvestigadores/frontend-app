8939976485ca5fc2a453be1dd7a48f32ca9831b3
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-05-30 14:28:52 -0500

    chore: Update profile data component to display user information dynamically

--- src/app/profile/presentation/components/profile-data/profile-data.component.ts
+++ src/app/profile/presentation/components/profile-data/profile-data.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+// profile-data.component.ts
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-profile-data',
   templateUrl: './profile-data.component.html',
   styleUrls: ['./profile-data.component.css']
 })
-export class ProfileDataComponent {
+export class ProfileDataComponent implements OnChanges {
+  @Input() user: any;
   mostrarBloqueEnPantallaPequena = false;
 
-  toggleVisibilidadEnPantallaPequena() {
-    this.mostrarBloqueEnPantallaPequena = !this.mostrarBloqueEnPantallaPequena;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user']) {
+    }
   }
 }
