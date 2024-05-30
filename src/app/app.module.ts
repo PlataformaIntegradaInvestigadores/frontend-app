cb7f37245c0609b51f62cfee5f22db1a0689c3e7
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-05-30 14:25:31 -0500

    chore: Update routing for profile page to include user ID parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProfilePageRoutingModule } from './profile/profile-page-routing.module';
 import { LoginComponent } from './auth/presentation/login/login.component';
 import { RegisterComponent } from './auth/presentation/register/register.component';
-import { AboutmeComponent } from './profile/presentation/components/aboutme/aboutme.component';
 import { NetworkComponent } from './profile/presentation/components/network/network.component';
 import { ContactComponent } from './profile/presentation/components/contact/contact.component';
 import { ArticleComponent } from './profile/presentation/components/article/article.component';
 import { FingerprintComponent } from './profile/presentation/components/fingerprint/fingerprint.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    AboutmeComponent,
     NetworkComponent,
     ContactComponent,
     ArticleComponent,
@@ -36,8 +35,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ProfilePageRoutingModule,
     FontAwesomeModule,
     NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
