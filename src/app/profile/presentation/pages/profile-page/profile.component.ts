5c7d9b01fdb27ad54eb20957ac3a74f488fa62c9
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-05-30 14:29:03 -0500

    chore: Update profile page to pass user data to child components

--- src/app/profile/presentation/pages/profile-page/profile.component.ts
+++ src/app/profile/presentation/pages/profile-page/profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+// profile.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/profile/domain/entities/user.service';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userId: number = 0;
+  user: any;
 
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.userId = +params['id'];
+      this.getUserData();
+    });
+  }
+
+  getUserData() {
+    this.userService.getUserById(this.userId).subscribe(data => {
+      this.user = data;
+    });
+  }
 }
