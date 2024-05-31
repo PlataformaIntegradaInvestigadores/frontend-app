26a860346c4a58523a1cd1672cad8a23625a4b4b
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-05-31 17:33:05 -0500

    feat: Add profile data form component
    This commit adds a new component called 'data-form' to handle the user profile data form. The component includes input fields for the user's first name, last name, institutional email, institution, investigation camp, Scopus ID, website, and profile picture. It also includes validation for the Scopus ID field to only allow numeric input. The form submission is handled by the 'onSubmit' method, which emits an event to notify the parent component. This new component enhances the user profile page by allowing users to update their profile information.
    
    Refactor recent user commits:
    - Update profile data component to include additional user details
    - Update profile data component to handle login status and form toggle
    - Add user ID to local storage upon successful login
    - Update login component to navigate to user's profile page after successful login
    - Remove console.log statement in AboutMeComponent
    - Add conditional rendering for own profile message
    - Update header component to add login and logout buttons
    
    Refactor recent repository commits:
    - Update profile page to include user data in profile component
    - Update profile page routing to use dynamic user ID parameter
    - Update summary component HTML and author list component CSS
    - Update background image size and position in search result component
    
    Chore recent repository commits:
    - Update npm dependency for jwt-decode to version 4.0.0
    - Update summary component HTML and author list component CSS

---
+++ src/app/profile/presentation/components/data-form/data-form.component.ts
@@ -0,0 +1,61 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-data-form',
+  templateUrl: './data-form.component.html',
+  styleUrls: ['./data-form.component.css']
+})
+export class DataFormComponent {
+
+  @Input() user: any;
+  @Output() formSubmitted = new EventEmitter<void>();
+
+  formData = {
+    first_name: '',
+    last_name: '',
+    scopus_id: '',
+    institution: '',
+    website: '',
+    investigation_camp: '',
+    profile_picture: '',
+    email_institution: '',
+  };
+
+  ngOnInit() {
+    if (this.user) {
+      this.formData = {
+        first_name: this.user.first_name || '',
+        last_name: this.user.last_name || '',
+        scopus_id: this.user.scopus_id || '',
+        institution: this.user.institution || '',
+        website: this.user.website || '',
+        investigation_camp: this.user.investigation_camp || '',
+        profile_picture: this.user.profile_picture || '',
+        email_institution: this.user.email_institution || '',
+      };
+    }
+  }
+
+  onSubmit() {
+    // Lógica para manejar el envío del formulario
+    console.log(this.formData);
+    this.formSubmitted.emit();
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.formData.profile_picture = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  preventNonNumeric(event: KeyboardEvent): void {
+    if (event.key < '0' || event.key > '9') {
+      event.preventDefault();
+    }
+  }
+}
