cb95c36608877b5f3f977676a5a7ee8f29c8e92c
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-05 08:47:12 -0500

    refactor: Add GroupService for creating groups
    This commit adds the GroupService to the profile domain. The GroupService is responsible for creating groups by making an HTTP POST request to the appropriate API endpoint. It includes error handling for both client-side and server-side errors. This enhancement improves the functionality of the application by allowing users to create groups.

---
+++ src/app/profile/domain/entities/group.service.ts
@@ -0,0 +1,40 @@
+// group.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/auth/domain/entities/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GroupService {
+    private apiUrl = `${environment.apiUrl}/groups/create/`;
+
+    constructor(private http: HttpClient, private authService: AuthService) { }
+
+    createGroup(groupData: { title: string, description: string }): Observable<any> {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        });
+
+        return this.http.post(this.apiUrl, groupData, { headers }).pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: any): Observable<never> {
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error instanceof ErrorEvent) {
+            // Client-side error
+            errorMessage = `Client-side error: ${error.error.message}`;
+        } else if (error.error) {
+            // Server-side error
+            errorMessage = `Server-side error: ${error.error.detail || error.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => new Error(errorMessage));
+    }
+}
