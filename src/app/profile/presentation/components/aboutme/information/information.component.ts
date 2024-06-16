95bbce4c51acfebbaba617790974efcba79ad81c
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-16 12:17:00 -0500

    refactor: Update About Me, Disciplines, and Contact Info components
    This commit refactors the About Me, Disciplines, and Contact Info components to improve the user experience and functionality. The changes include:
    - Replacing the existing HTML code in the Information component with the new About Me, Disciplines, and Contact Info components.
    - Adding functionality to the About Me component to allow users to view and edit their personal information.
    - Adding functionality to the Disciplines component to allow users to showcase their expertise in different disciplines.
    - Adding functionality to the Contact Info component to allow users to view and manage their contact information.
    
    These enhancements provide users with a convenient way to manage and display their profile information, improving the overall user experience.

--- src/app/profile/presentation/components/aboutme/information/information.component.ts
+++ src/app/profile/presentation/components/aboutme/information/information.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/domain/entities/auth.service';
+import { InformationService } from 'src/app/profile/domain/entities/information.service';
+import { UserDataService } from 'src/app/profile/domain/entities/user_data.service';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
   styleUrls: ['./information.component.css']
 })
-export class InformationComponent {
+export class InformationComponent implements OnInit {
 
+  userInfo: any = {};
+  disciplines: string[] = [];
+  contactInfo: { type: string, value: string }[] = [];
+
+  user: any;
+  isloggedIn: boolean = false;
+
+  constructor(
+    private informationService: InformationService,
+    private authService: AuthService,
+    private userDataService: UserDataService
+  ) { }
+
+  ngOnInit(): void {
+    this.userDataService.getUser().subscribe(user => {
+      this.user = user;
+      if (this.user) {
+        this.fetchPublicInformation(this.user.user_id);
+      }
+    });
+
+    this.isloggedIn = this.authService.isLoggedIn();
+  }
+
+  fetchPublicInformation(userId: string): void {
+    this.informationService.getPublicInformation(userId).subscribe(info => {
+      this.userInfo = info || {};
+      this.disciplines = info.disciplines || [];
+      this.contactInfo = info.contact_info || [];
+    });
+  }
+
+  saveAboutMe(aboutMe: string): void {
+    this.informationService.updateInformation({ about_me: aboutMe }).subscribe(response => {
+      this.userInfo.about_me = response.about_me;
+    });
+  }
+
+  saveDisciplines(disciplines: string[]): void {
+    this.informationService.updateInformation({ disciplines }).subscribe(response => {
+      this.disciplines = response.disciplines;
+    });
+  }
+
+  saveContactInfo(contactInfo: { type: string, value: string }[]): void {
+    this.informationService.updateInformation({ contact_info: contactInfo }).subscribe(response => {
+      this.contactInfo = response.contact_info;
+    });
+  }
+
+  toggleEdit(): void {
+    // Implement logic to handle toggle edit state if needed
+  }
 }
