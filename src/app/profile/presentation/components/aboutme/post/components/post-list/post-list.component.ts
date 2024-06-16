1513c66829495e0f37430003f00c3fde6a9ecfb9
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-16 12:17:26 -0500

    feat: Add PostListComponent
    This commit adds the PostListComponent, which displays a list of posts in the About Me section of the profile page. The component receives an array of posts and the user object as inputs, and emits a deletePost event when a post is deleted. This addition enhances the functionality of the profile module and improves the overall user experience.

---
+++ src/app/profile/presentation/components/aboutme/post/components/post-list/post-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from 'src/app/profile/domain/entities/post.interface';
+import { PostService } from 'src/app/profile/domain/entities/post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  @Input() posts: Post[] = [];
+  @Input() user: any;
+  @Output() deletePost = new EventEmitter<string>();
+
+  constructor(private postService: PostService) { }
+
+  onDeletePost(postId: string) {
+    this.deletePost.emit(postId);
+  }
+
+  loadPosts(userId: string): void {
+    this.postService.getPosts(userId).subscribe(
+      posts => {
+        this.posts = posts;
+      },
+      error => {
+        console.error('Error loading posts:', error);
+      }
+    );
+  }
+}
