8140f16f8f7b730f7daeaa5606232718cd2418f1
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-20 11:18:40 -0500

    refactor: Update aboutme component to improve responsiveness
    This commit updates the aboutme component to improve responsiveness by implementing conditional rendering based on the screen size. The component now displays different layouts for mobile and desktop views, providing a better user experience. The changes include rearranging the order of child components and adjusting their column spans accordingly. Additionally, the code has been refactored to use ng-container and ng-template for conditional rendering, improving code readability and maintainability.

--- src/app/profile/presentation/components/aboutme/aboutme.component.ts
+++ src/app/profile/presentation/components/aboutme/aboutme.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/domain/services/auth.service';
 import { UserDataService } from 'src/app/profile/domain/services/user_data.service';
 
@@ -10,6 +10,8 @@ import { UserDataService } from 'src/app/profile/domain/services/user_data.servi
 export class AboutMeComponent implements OnInit {
   user: any;
   isloggedIn: boolean = false;
+  isMobileView = false;
+
 
   constructor(private userDataService: UserDataService, private authService: AuthService) { }
 
@@ -18,5 +20,14 @@ export class AboutMeComponent implements OnInit {
       this.user = user;
     });
     this.isloggedIn = this.authService.isLoggedIn();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobileView = window.innerWidth < 768; // Define the breakpoint for mobile view
   }
 }
