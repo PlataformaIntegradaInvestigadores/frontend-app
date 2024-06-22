7230cc4d2adfcc9453ab2ed78663bcb4a91aaea0
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-22 15:21:31 -0500

    refactor: Update about-me-profile component to improve responsiveness and layout
    The about-me-profile component has been updated to improve responsiveness and layout. The changes include adjusting the alignment of the "About Me" section and the edit button, as well as updating the styles of the text and textarea elements. These updates enhance the user experience and ensure a better presentation of the user's profile information.

--- src/app/profile/presentation/components/aboutme/information/components/about-me-profile/about-me-profile.component.ts
+++ src/app/profile/presentation/components/aboutme/information/components/about-me-profile/about-me-profile.component.ts
@@ -1,22 +1,29 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { UserInfo } from 'src/app/profile/domain/entities/user.interfaces';
 
 @Component({
   selector: 'app-about-me-profile',
   templateUrl: './about-me-profile.component.html',
   styleUrls: ['./about-me-profile.component.css']
 })
-export class AboutMeProfileComponent {
-  @Input() userInfo: any;
+export class AboutMeProfileComponent implements OnChanges {
+  @Input() userInfo: UserInfo = this.initializeUserInfo();
   @Input() isOwnProfile?: boolean;
   @Output() saveAboutMe = new EventEmitter<string>();
   @Output() toggleEdit = new EventEmitter<void>();
 
   isEditing: boolean = false;
-  editableUserInfo: any = {};
+  editableUserInfo: UserInfo = this.initializeUserInfo();
   saveMessage: string = '';
 
-  ngOnChanges(): void {
-    this.editableUserInfo = { ...this.userInfo };
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['userInfo'] && changes['userInfo'].currentValue) {
+      this.editableUserInfo = { ...this.userInfo };
+    }
+  }
+
+  private initializeUserInfo(): UserInfo {
+    return { about_me: '', disciplines: [], contact_info: [] };
   }
 
   /**
@@ -31,7 +38,7 @@ export class AboutMeProfileComponent {
    * Guarda la información de "Sobre mí" y emite el evento correspondiente.
    */
   save(): void {
-    this.saveAboutMe.emit(this.editableUserInfo.about_me);
+    this.saveAboutMe.emit(this.editableUserInfo.about_me || '');
     this.isEditing = false;
     this.displaySaveMessage();
   }
@@ -51,6 +58,14 @@ export class AboutMeProfileComponent {
     this.saveMessage = 'Changes saved successfully!';
     setTimeout(() => {
       this.saveMessage = '';
-    }, 3000); // Hide message after 3 seconds
+    }, 3000); // Oculta el mensaje después de 3 segundos
+  }
+
+  /**
+   * Verifica si hay contenido en "Sobre mí".
+   * @returns Verdadero si hay contenido, falso en caso contrario.
+   */
+  hasAboutMeContent(): boolean {
+    return !!this.userInfo.about_me && this.userInfo.about_me.length > 0;
   }
 }
