501893036630df91734f38143656945b298206cb
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-22 15:34:52 -0500

    refactor: Update UserDataService to use User interface
    The UserDataService in user_data.service.ts has been updated to use the User interface from user.interfaces.ts. This change ensures that the changeUser and setUser methods accept and emit the correct type of data. Additionally, the isCurrentUser method has been added to verify if the current user is the owner of the profile. This update improves the code structure and type safety.

--- src/app/profile/domain/services/user_data.service.ts
+++ src/app/profile/domain/services/user_data.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/profile/domain/entities/user.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
-  private userSource = new BehaviorSubject<any>(null);
-  currentUser = this.userSource.asObservable();
-  private userSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  public user$: Observable<any> = this.userSubject.asObservable();
+  private userSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  public user$: Observable<User | null> = this.userSubject.asObservable();
 
   constructor() { }
 
@@ -16,23 +15,36 @@ export class UserDataService {
    * Cambia el usuario actual.
    * @param user - El nuevo usuario.
    */
-  changeUser(user: any): void {
-    this.userSource.next(user);
+  changeUser(user: User | null): void {
+    this.userSubject.next(user);
   }
 
   /**
    * Establece el usuario actual.
    * @param user - El usuario a establecer.
    */
-  setUser(user: any): void {
+  setUser(user: User | null): void {
+    if (user && user.id && !user.isOwnProfile) {
+      user.isOwnProfile = this.isCurrentUser(user.id);
+    }
     this.userSubject.next(user);
+  }
+
+  /**
+   * Verifica si el usuario actual es el propietario del perfil.
+   * @param userId - El ID del usuario.
+   * @returns Verdadero si el usuario es el propietario del perfil, falso en caso contrario.
+   */
+  private isCurrentUser(userId: string): boolean {
+    const currentUserId = localStorage.getItem('userId');
+    return currentUserId === userId;
   }
 
   /**
    * Obtiene el usuario actual como un Observable.
    * @returns Un Observable que emite el usuario actual.
    */
-  getUser(): Observable<any> {
+  getUser(): Observable<User | null> {
     return this.user$;
   }
 }
