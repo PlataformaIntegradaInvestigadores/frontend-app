dd72fa6afc9a41b77a93c13606e7ae55f1096ecc
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-22 15:31:32 -0500

    refactor: Improve form validation error messages
    The getFormValidationErrors function in form-utils.ts has been updated to improve the error messages displayed for form validation. The changes include refining the error messages for required fields, minimum length, pattern matching, password mismatch, and minimum age. These updates enhance the user experience by providing clearer and more informative error messages.

--- src/app/auth/domain/entities/form-utils.ts
+++ src/app/auth/domain/entities/form-utils.ts
@@ -6,39 +6,39 @@ import { FormGroup } from '@angular/forms';
  * @returns Una lista de mensajes de error.
  */
 export function getFormValidationErrors(form: FormGroup): string[] {
-    const messages: string[] = [];
-    Object.keys(form.controls).forEach(key => {
-        const controlErrors = form.get(key)?.errors;
-        if (controlErrors) {
-            Object.keys(controlErrors).forEach(keyError => {
-                switch (keyError) {
-                    case 'required':
-                        messages.push(`${getControlLabel(key)} is required.`);
-                        break;
-                    case 'minlength':
-                        messages.push(`${getControlLabel(key)} must be at least 8 characters long.`);
-                        break;
-                    case 'pattern':
-                        if (key === 'password') {
-                            messages.push(`${getControlLabel(key)} must contain at least one uppercase letter, one lowercase letter, one number, and one special character.`);
-                        } else if (key === 'scopus_id') {
-                            messages.push(`${getControlLabel(key)} must be a positive number.`);
-                        }
-                        break;
-                    case 'mismatch':
-                        messages.push(`Passwords do not match.`);
-                        break;
-                    case 'minAge':
-                        messages.push(`You must be at least 18 years old to register.`);
-                        break;
-                    default:
-                        messages.push(`Invalid value in ${getControlLabel(key)}.`);
-                        break;
-                }
-            });
+  const messages: string[] = [];
+  Object.keys(form.controls).forEach(key => {
+    const controlErrors = form.get(key)?.errors;
+    if (controlErrors) {
+      Object.keys(controlErrors).forEach(keyError => {
+        switch (keyError) {
+          case 'required':
+            messages.push(`${getControlLabel(key)} is required.`);
+            break;
+          case 'minlength':
+            messages.push(`${getControlLabel(key)} must be at least 8 characters long.`);
+            break;
+          case 'pattern':
+            if (key === 'password') {
+              messages.push(`${getControlLabel(key)} must contain at least one uppercase letter, one lowercase letter, one number, and one special character.`);
+            } else if (key === 'scopus_id') {
+              messages.push(`${getControlLabel(key)} must be a positive number.`);
+            }
+            break;
+          case 'mismatch':
+            messages.push(`Passwords do not match.`);
+            break;
+          case 'minAge':
+            messages.push(`You must be at least 18 years old to register.`);
+            break;
+          default:
+            messages.push(`Invalid value in ${getControlLabel(key)}.`);
+            break;
         }
-    });
-    return messages;
+      });
+    }
+  });
+  return messages;
 }
 
 /**
@@ -47,15 +47,15 @@ export function getFormValidationErrors(form: FormGroup): string[] {
  * @returns La etiqueta del control de formulario.
  */
 function getControlLabel(controlName: string): string {
-    const controlLabels: { [key: string]: string } = {
-        first_name: 'First Name',
-        last_name: 'Last Name',
-        email: 'Email',
-        password: 'Password',
-        confirm_password: 'Confirm Password',
-        birthday: 'Birthday',
-        scopus_id: 'Scopus ID',
-        agree_terms: 'Agree to Terms'
-    };
-    return controlLabels[controlName] || controlName;
+  const controlLabels: { [key: string]: string } = {
+    first_name: 'First Name',
+    last_name: 'Last Name',
+    email: 'Email',
+    password: 'Password',
+    confirm_password: 'Confirm Password',
+    birthday: 'Birthday',
+    scopus_id: 'Scopus ID',
+    agree_terms: 'Agree to Terms'
+  };
+  return controlLabels[controlName] || controlName;
 }
