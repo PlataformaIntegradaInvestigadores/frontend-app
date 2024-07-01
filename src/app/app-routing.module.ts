2371aa4b6cc763717e1e39e98df0bced2edefcce
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-07-01 15:21:08 -0500

    add(admin module)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/presentation/login/login.component';
 import { RegisterComponent } from './auth/presentation/register/register.component';
+import { AdminComponent } from './search-engine/presentation/admin/pages/login/admin.component';
 
 
 const routes: Routes = [
@@ -21,7 +22,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('src/app/search-engine/presentation/home-page/home-page.module').then(m => m.HomePageModule)
   },
-  
+  {
+    path:'admin',
+    loadChildren:() => import("src/app/search-engine/presentation/admin/admin.module").then( m => m.AdminModule)
+  },
 
   /* Siempre al ultimo */
   {
