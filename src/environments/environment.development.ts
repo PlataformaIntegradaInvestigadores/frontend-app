383aaab229f3d8986b79be8665d6d359bc5a9ae0
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-07-16 14:24:02 -0500

    refactor: change port for api centinela

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: false,
-  apiCentinela: "http://localhost:8010/",
+  apiCentinela: "http://localhost:8001/",
   wsUrl: 'ws://172.28.36.130:8000/ws',
   apiUrl: 'http://172.28.36.130:8000/api',
 };
-
-/* export const environment = {
-  production: false,
-  apiCentinela: "http://localhost:8010/",
-  wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiUrl: 'http://172.28.36.130:8000/api',
-};
- */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://172.28.36.130:8000/api',
   wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiCentinela: "http://localhost:8010/",
+  apiCentinela: "http://localhost:8001/",
 };
-
-/* export const environment = {
-  production: false,
-  apiCentinela: "http://localhost:8010/",
-  wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiUrl: 'http://172.28.36.130:8000/api',
-}; */
