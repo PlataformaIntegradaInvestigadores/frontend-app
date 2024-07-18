24ef6817c4cad563c97d64d17f900aaa5b09fb76
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-07-18 15:52:25 -0500

    refactor: added support for additional contact types in detectContactType function

--- src/app/profile/presentation/components/aboutme/information/components/contact-info/contact-info.component.ts
+++ src/app/profile/presentation/components/aboutme/information/components/contact-info/contact-info.component.ts
@@ -70,63 +70,75 @@ export class ContactInfoComponent implements OnChanges {
     }
   }
 
-  /**
-   * Detecta el tipo de contacto basado en el valor proporcionado.
-   * @param value - El valor de contacto.
-   * @returns El tipo de contacto detectado.
-   */
-  detectContactType(value: string): string {
-    if (/facebook\.com/.test(value)) {
-      return 'facebook';
-    } else if (/twitter\.com/.test(value) || /x\.com/.test(value)) {
-      return 'x';
-    } else if (/linkedin\.com/.test(value)) {
-      return 'linkedin';
-    } else if (/^(http|https):\/\/[^\s$.?#].[^\s]*$/.test(value)) {
-      return 'website';
-    } else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
-      return 'email';
-    } else if (/^\d{10}$/.test(value)) {
-      return 'phone';
-    } else {
-      return 'other';
-    }
+/**
+ * Detecta el tipo de contacto basado en el valor proporcionado.
+ * @param value - El valor de contacto.
+ * @returns El tipo de contacto detectado.
+ */
+detectContactType(value: string): string {
+  if (/facebook\.com/.test(value)) {
+    return 'facebook';
+  } else if (/twitter\.com/.test(value) || /x\.com/.test(value)) {
+    return 'x';
+  } else if (/linkedin\.com/.test(value)) {
+    return 'linkedin';
+  } else if (/github\.com/.test(value)) {
+    return 'github';
+  } else if (/youtube\.com\/@[\w\-]+/.test(value)) {
+    return 'youtube';
+  } else if (/orcid\.org(\/\d{4}-\d{4}-\d{4}-\d{3}[0-9X])?/.test(value)) {
+    return 'orcid';
+  } else if (/^(http|https):\/\/[^\s$.?#].[^\s]*$/.test(value)) {
+    return 'website';
+  } else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+    return 'email';
+  } else if (/^\d{10}$/.test(value)) {
+    return 'phone';
+  } else {
+    return 'other';
   }
+}
 
-  /**
-   * Verifica si un valor es una URL.
-   * @param value - El valor a verificar.
-   * @returns Verdadero si el valor es una URL, falso en caso contrario.
-   */
-  isUrl(value: string): boolean {
-    const urlPattern = /^(http|https):\/\/[^\s$.?#].[^\s]*$/;
-    const socialMediaPattern = /(facebook\.com|twitter\.com|x\.com|linkedin\.com)/;
-    return urlPattern.test(value) || socialMediaPattern.test(value);
-  }
+/**
+ * Verifica si un valor es una URL.
+ * @param value - El valor a verificar.
+ * @returns Verdadero si el valor es una URL, falso en caso contrario.
+ */
+isUrl(value: string): boolean {
+  const urlPattern = /^(http|https):\/\/[^\s$.?#].[^\s]*$/;
+  const socialMediaPattern = /(facebook\.com|twitter\.com|x\.com|linkedin\.com|github\.com|youtube\.com|youtu\.be|orcid\.org)/;
+  return urlPattern.test(value) || socialMediaPattern.test(value);
+}
 
-  /**
-   * Obtiene el ícono correspondiente al tipo de contacto.
-   * @param type - El tipo de contacto.
-   * @returns El nombre de la clase del ícono.
-   */
-  getContactIcon(type: string): string {
-    switch (type.toLowerCase()) {
-      case 'phone':
-        return 'fa-solid fa-phone';
-      case 'email':
-        return 'fa-solid fa-envelope';
-      case 'website':
-        return 'fa-solid fa-link';
-      case 'facebook':
-        return 'fa-brands fa-facebook';
-      case 'x':
-        return 'fa-brands fa-x-twitter';
-      case 'linkedin':
-        return 'fa-brands fa-linkedin';
-      default:
-        return 'fa-solid fa-info-circle';
-    }
+/**
+ * Obtiene el ícono correspondiente al tipo de contacto.
+ * @param type - El tipo de contacto.
+ * @returns El nombre de la clase del ícono.
+ */
+getContactIcon(type: string): string {
+  switch (type.toLowerCase()) {
+    case 'phone':
+      return 'fa-solid fa-phone';
+    case 'email':
+      return 'fa-solid fa-envelope';
+    case 'website':
+      return 'fa-solid fa-link';
+    case 'facebook':
+      return 'fa-brands fa-facebook';
+    case 'x':
+      return 'fa-brands fa-x-twitter';
+    case 'linkedin':
+      return 'fa-brands fa-linkedin';
+    case 'github':
+      return 'fa-brands fa-github';
+    case 'youtube':
+      return 'fa-brands fa-youtube';
+    case 'orcid':
+      return 'fa-brands fa-orcid';
+    default:
+      return 'fa-solid fa-info-circle';
   }
+}
 
   /**
    * Muestra un mensaje de guardado exitoso.
