5c64d0325233d6b2470779c2e862489c952f10db
JoffreC <joffre.condor@epn.edu.ec>
2024-07-18 15:46:07 -0500

    feat(visuals): new color palette

--- src/app/shared/domain/services/visuals.service.ts
+++ src/app/shared/domain/services/visuals.service.ts
@@ -3,13 +3,15 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DashboardCounts, Word, YearsResponse} from "../../interfaces/dashboard.interface";
 import {environment} from "../../../../environments/environment";
+import {Color, ScaleType} from "@swimlane/ngx-charts";
+import * as d3 from 'd3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisualsService {
 
-  private apiUrl = environment.apiCentinela+'api/v1/dashboard';
+  private apiUrl = environment.apiCentinela + 'api/v1/dashboard';
 
   constructor(private http: HttpClient) {
   }
@@ -24,11 +26,26 @@ export class VisualsService {
     return this.http.get<Word[]>(`${this.apiUrl}/country/get_topics/`, {params});
   }
 
-  getProvinces():Observable<any>{
+  getProvinces(): Observable<any> {
     return this.http.get<YearsResponse[]>(`${this.apiUrl}/province/get_provinces/`);
   }
 
+  generateColorDomain(length: number): d3.ScaleSequential<string> {
+    return d3.scaleSequential(d3.interpolateViridis).domain([0, length - 1]);
+  }
 
-
-
+  createColorScheme(length: number): Color {
+    const colorScale = this.generateColorDomain(length);
+    const colors: string[] = [];
+    for (let i = 0; i < length; i++) {
+      colors.push(colorScale(i) as unknown as string);
+    }
+    return {
+      name: 'custom',
+      selectable: true,
+      group: ScaleType.Ordinal,
+      domain: colors
+    };
+  }
 }
+
