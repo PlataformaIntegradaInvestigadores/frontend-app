210d67adfb3c6706ddb7f9efb976f4860383e83c
JoffreC <joffre.condor@epn.edu.ec>
2024-07-18 17:31:49 -0500

    fix: bug in graphics resolved

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiCentinela: "http://172.28.36.130:8001/",
+  apiCentinela: "http://localhost:8000/",
+  // apiCentinela: "http://172.28.36.130:8001/",
   wsUrl: 'ws://172.28.36.130:8000/ws',
   apiUrl: 'http://172.28.36.130:8000/api',
 };
