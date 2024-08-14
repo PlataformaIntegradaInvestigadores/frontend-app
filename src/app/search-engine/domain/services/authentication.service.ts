27954e04a8f94dd9ba23e073aaaaa740b3a73545
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-08-13 19:23:40 -0500

    chore: Update API endpoint and authentication service

---
+++ src/app/search-engine/domain/services/authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { catchError, map, Observable, of } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthenticationService {
+  rootURL:string = environment.apiCentinela;
+  constructor(private httpClient: HttpClient) {
+   }
+
+  login(username: string, password: string) :Observable<any> {
+    return this.httpClient.post(`${this.rootURL}api/v1/auth/login/`, { username, password })
+    .pipe(
+      map((response) => {
+        return response
+      })
+    )
+  }
+
+  isAuthenticated() {
+    console.log(localStorage.getItem('authState'));
+    return !!localStorage.getItem('authState');
+  }
+}
