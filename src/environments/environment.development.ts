aeae737545b7b66ae5eff48232ba5a00f3f10dd1
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-09-04 08:39:05 -0500

    chore: Update API endpoint in development environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiCentinela: "http://localhost:8000/",
+  apiCentinela: "http://localhost:8001/",
   wsUrl: 'ws://localhost:8000/ws',
   apiUrl: 'http://localhost:8000/api',
 };
