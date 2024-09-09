70784015dc1a0cd3524e97c8ef0a7126c76b7398
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-09-09 16:57:05 -0500

    refactor: api centinela url

--- src/app/search-engine/domain/services/dashboard-admin.service.ts
+++ src/app/search-engine/domain/services/dashboard-admin.service.ts
@@ -13,34 +13,34 @@ export class DashboardAdminService {
   constructor(private httpClient: HttpClient) { }
 
   getAuthorComparator(): Observable<AuthorComparator> {
-    return this.httpClient.get<AuthorComparator>(`${this.rootURL}api/v1/dashboard/information/get_authors_comparator/`);
+    return this.httpClient.get<AuthorComparator>(`${this.rootURL}/v1/dashboard/information/get_authors_comparator/`);
   }
 
   getArticlesComparator(): Observable<ArticleComparator> {
-    return this.httpClient.get<ArticleComparator>(`${this.rootURL}api/v1/dashboard/information/get_articles_comparator/`);
+    return this.httpClient.get<ArticleComparator>(`${this.rootURL}/v1/dashboard/information/get_articles_comparator/`);
   }
 
   getmodelCorpusObserver(): Observable<ModelCorpusObserver> {
-    return this.httpClient.get<ModelCorpusObserver>(`${this.rootURL}api/v1/dashboard/information/tfidf_model_corpus/`);
+    return this.httpClient.get<ModelCorpusObserver>(`${this.rootURL}/v1/dashboard/information/tfidf_model_corpus/`);
   }
 
   generateCorpus(): Observable<Status> {
-    return this.httpClient.post<Status>(`${this.rootURL}api/v1/generate-corpus/`,{});
+    return this.httpClient.post<Status>(`${this.rootURL}/v1/generate-corpus/`,{});
   }
 
   generateModel(): Observable<Status> {
-    return this.httpClient.post<Status>(`${this.rootURL}api/v1/generate-model/`, {});
+    return this.httpClient.post<Status>(`${this.rootURL}/v1/generate-model/`, {});
   }
 
   getNoSqlDbYears(): Observable<YearsResponse[]> {
-    return this.httpClient.get<YearsResponse[]>(`${this.rootURL}api/v1/dashboard/country/get_last_years/`);
+    return this.httpClient.get<YearsResponse[]>(`${this.rootURL}/v1/dashboard/country/get_last_years/`);
   }
   getNoSqlDbCounts(year: number): Observable<DashboardCounts> {
     let params = new HttpParams().set('year', year.toString());
-    return this.httpClient.get<DashboardCounts>(`${this.rootURL}api/v1/dashboard/country/get_acumulated/`, {params});
+    return this.httpClient.get<DashboardCounts>(`${this.rootURL}/v1/dashboard/country/get_acumulated/`, {params});
   }
 
   populateNoSqlDb(): Observable<Status> {
-    return this.httpClient.post<Status>(`${this.rootURL}api/v1/dashboard/populate`,{});
+    return this.httpClient.post<Status>(`${this.rootURL}/v1/dashboard/populate`,{});
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -9,6 +9,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://172.28.36.130:8000/api',
   wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiCentinela: "http://172.28.36.130:8001/",
+  apiCentinela: "http://172.28.36.130:8001/api",
 };
 
