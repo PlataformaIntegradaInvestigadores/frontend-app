4c9a644d4e56b0e4214fd765b2c58b0c1dd0743d
Mateo <vimofama@outlook.com>
2024-10-21 16:41:40 -0500

    Refactorizar configuración de entorno y archivo .gitignore

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://172.28.36.130:8000/api',
-  wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiCentinela: "http://172.28.36.130:8001/api-se",
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,35 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://172.28.36.130:8000/api',
-  wsUrl: 'ws://172.28.36.130:8000/ws',
-  apiCentinela: "http://172.28.36.130:8001/api-se",
-};
+import 'dotenv/config';
+import * as joi from 'joi';
 
+interface EnvVars {
+  PRODUCTION: boolean;
+  CONSENSUS_URL: string;
+  WS_CONSENSUS_URL: string;
+  SEARCH_URL: string;
+}
+
+const envSchema = joi
+  .object({
+    PRODUCTION: joi.boolean().required(),
+    CONSENSUS_URL: joi.string().required(),
+    WS_CONSENSUS_URL: joi.string().required(),
+    SEARCH_URL: joi.string().required()
+  })
+  .unknown(true);
+
+const { error, value } = envSchema.validate({
+  ...process.env
+});
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const environment = {
+  production: envVars.PRODUCTION,
+  apiUrl: envVars.CONSENSUS_URL,
+  wsUrl: envVars.WS_CONSENSUS_URL,
+  apiCentinela: envVars.SEARCH_URL,
+};
