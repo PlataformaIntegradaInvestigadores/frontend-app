48daef91f73e40d9eeb7ec7cf3bdf49be7ba2554
Mateo <vimofama@outlook.com>
2025-02-19 08:38:26 -0500

    update environment URLs to local development endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,11 +4,3 @@
    wsUrl: 'ws://localhost:8000/ws',
    apiUrl: 'http://localhost:8000/api',
  };
-
-/* export const environment = {
-  production: false,
-  apiUrl: 'https://centinela.epn.edu.ec/api',
-  wsUrl: 'wss://centinela.epn.edu.ec/ws',
-  apiCentinela: "https://centinela.epn.edu.ec/api-se",
-};
- */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
-/* export const environment = {
-  production: false,
+export const environment = {
+  production: true,
   apiUrl: 'https://centinela.epn.edu.ec/api',
   wsUrl: 'wss://centinela.epn.edu.ec/ws',
   apiCentinela: "https://centinela.epn.edu.ec/api-se",
 };
- */
-export const environment = {
-  production: false,
-  apiCentinela: "http://localhost:8001/api-se",
-  wsUrl: 'ws://localhost:8000/ws',
-  apiUrl: 'http://localhost:8000/api',
-};
+
