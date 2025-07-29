cb142fcd471def4ac47b19d32a7c0ca2b014c09a
KennyP <kennyp41234@gmail.com>
2025-07-29 12:46:04 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiCentinela: "http://centinela.epn.edu.ec/api-se",
+  apiCentinela: "https://centinela.epn.edu.ec/api-se",
   wsUrl: 'wss://centinela.epn.edu.ec/ws',
   apiUrl: 'https://centinela.epn.edu.ec/api',
 };
