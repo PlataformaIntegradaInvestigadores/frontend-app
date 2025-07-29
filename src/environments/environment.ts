b151733ae9aa08b1be134303151945ada870becb
KennyP <kennyp41234@gmail.com>
2025-07-29 12:44:30 -0500

    minor fix environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiCentinela: "http://centinela.epn.edu.ec:8001/api-se",
-  wsUrl: 'wss://centinela.epn.edu.ec:8000/ws',
-  apiUrl: 'https://centinela.epn.edu.ec:8000/api',
+  apiCentinela: "http://centinela.epn.edu.ec/api-se",
+  wsUrl: 'wss://centinela.epn.edu.ec/ws',
+  apiUrl: 'https://centinela.epn.edu.ec/api',
 };
