3685aa682a4fa330c3d9ef0233abe1c0045f5e80
Ariel <ariel.amaguana91@gmail.com>
2026-02-20 21:57:10 -0500

    fix: Agregando campos en las variables de entorno para despliegue

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,10 @@
 export const environment = {
-  production: true,
-  apiCentinela: "https://centinela.epn.edu.ec:8001/api-se",
-  wsUrl: 'wss://centinela.epn.edu.ec:8000/ws',
-  apiUrl: 'https://centinela.epn.edu.ec:8000/api',
+  production: false,
+  apiUrl: '/api',
+  wsUrl: 'ws://192.168.0.109:8082/ws',
+  apiCentinela: '/api-se',
+  //production: true,
+  //apiCentinela: "https://centinela.epn.edu.ec:8001/api-se",
+  //wsUrl: 'wss://centinela.epn.edu.ec:8000/ws',
+  //apiUrl: 'https://centinela.epn.edu.ec:8000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: false,
-  apiCentinela: 'http://localhost:8001/api-se',
-  wsUrl: 'ws://localhost:8000/ws',
-  apiUrl: 'http://localhost:8000/api',
+  apiUrl: '/api',
+  wsUrl: 'ws://192.168.0.109:8082/ws',
+  apiCentinela: '/api-se',
+  //apiCentinela: 'http://localhost:8001/api-se',
+  //wsUrl: 'ws://localhost:8000/ws',
+  //apiUrl: 'http://localhost:8000/api',
 };
