efb7a5f172bdbc6316f8220288e2d5a0c7e1883c
Ariel Justin Amaguaña Toapanta <ariel.amaguana@epn.edu.ec>
2026-06-07 13:55:15 -0500

    chore(frontend): route search v2 through gateway

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiCentinela: 'http://host.docker.internal:8002/api-se',
+  apiCentinela: '/api-se',
   wsUrl: 'ws://localhost:8082/ws',
   apiUrl: '/api',
 };
