36257fb317bb0abaa9d948ef902202a58b1bf762
Asdk22-eu <andres.suarez@epn.edu.ec>
2026-07-24 22:40:09 -0500

    fix: agregar HttpClientTestingModule al spec de RecommendationsPageComponent
    Faltaba el mismo fix que ya se aplico a las otras 55 specs de esta
    rama (HttpClientTestingModule/FormsModule/RouterTestingModule/
    NO_ERRORS_SCHEMA) porque este spec se creo despues de esos commits.
    Sin esto, CI fallaba con NullInjectorError: No provider for HttpClient.

--- src/app/recommendations/presentation/pages/recommendations-page/recommendations-page.component.spec.ts
+++ src/app/recommendations/presentation/pages/recommendations-page/recommendations-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecommendationsPageComponent } from './recommendations-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('RecommendationsPageComponent', () => {
   let component: RecommendationsPageComponent;
@@ -8,7 +12,9 @@ describe('RecommendationsPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RecommendationsPageComponent]
+      declarations: [RecommendationsPageComponent],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule, RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(RecommendationsPageComponent);
     component = fixture.componentInstance;
